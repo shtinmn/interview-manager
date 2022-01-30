b9b5eadbf53fd3efcff395b16b119bbe032538a9
Maksim Shtin <shtinmn@gmail.com>
2022-01-31 00:17:17 +0300

    feat: добавлен mui

---
+++ pages/questions.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+import { Container } from '@mui/material'
+
+import Button from '@mui/material/Button'
+
+export default function QuestionsPage() {
+  return (
+    <Container maxWidth={false}>
+      <Button variant="contained">Hello World</Button>
+    </Container>
+  )
+}
